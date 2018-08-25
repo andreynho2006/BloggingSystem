474f9be4959d698436b5d59d04fbce0f7635ed40
andreynho2006 <andreynho2006@gmail.com>
2018-08-25 18:54:35 +0300

    make a new entry on button save click

--- models/Blog_Entry_Table.class.php
+++ models/Blog_Entry_Table.class.php
@@ -1,24 +1,23 @@
 <?php
 
 class Blog_Entry_Table {
-
     private $db;
-
+    //notice there are two underscore characters in __construct
     public function __construct ( $db ) {
         $this->db = $db;
     }
 
     public function saveEntry ( $title, $entry ) {
-        $entrySQL = "INSERT INTO blog_entry ( title, entry_text ) 
+        $entrySQL = "INSERT INTO blog_entry ( title, entry_text )
                      VALUES ( '$title', '$entry' )";
         $entryStatement = $this->db->prepare( $entrySQL );
 
-        try {
-            $entryStatement->execute();
-        } catch ( Exception $e ) {
-            $msg = "<p>Tou tried to run this sql: $entrySQL</p>
-                    <p>Exception: $e</p>";
-            trigger_error($msg);
+    try{
+        $entryStatement->execute();
+    } catch (Exception $e){
+        $msg = "<p>You tried to run this sql: $entrySQL<p>
+               <p>Exception: $e</p>";
+        trigger_error($msg);
         }
     }
 }

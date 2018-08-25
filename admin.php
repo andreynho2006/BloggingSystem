93ca88dde615c868987eb14ce8acb1d044a0ac23
andreynho2006 <andreynho2006@gmail.com>
2018-08-25 08:53:05 +0300

    make folder for models, views, controllers, css and the admin page

---
+++ admin.php
@@ -0,0 +1,13 @@
+<?php
+
+error_reporting( E_ALL );
+ini_set( "display_errors", 1 );
+
+include_once "models/Page_Data.class.php";
+$pageData = new Page_Data();
+$pageData->title = "PHP/MySQL blog";
+$pageData->addCSS("css/blog.css");
+$pageData->content = "<h1>YES!</h1>";
+
+$page = include_once "views/page.php";
+echo $page;

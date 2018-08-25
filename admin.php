c5953980ca98d78e4c431f56946082a7fbc05b98
andreynho2006 <andreynho2006@gmail.com>
2018-08-25 17:57:03 +0300

    make a var and set it  to the value of the $_GET['page'] and load a controller to that page

--- admin.php
+++ admin.php
@@ -4,10 +4,24 @@ error_reporting( E_ALL );
 ini_set( "display_errors", 1 );
 
 include_once "models/Page_Data.class.php";
+
 $pageData = new Page_Data();
 $pageData->title = "PHP/MySQL blog";
 $pageData->addCSS("css/blog.css");
-$pageData->content = "<h1>YES!</h1>";
+//load navigation
+$pageData->content = include_once "views/admin/admin-navigation.php";
+
+$navigationIsClicked = isset( $_GET['page']);
+if ( $navigationIsClicked ) {
+    //prepare to load corresponding controller
+    $contrl = $_GET['page'];
+} else {
+    // prepare to load default controller
+    $contrl = "entries";
+}
+
+//load the controller
+$pageData->content .= include_once "controllers/admin/$contrl.php";
 
 $page = include_once "views/page.php";
 echo $page;

06114d5fc59f57ece7cc3903dbacaa5bebdcb7ba
andreynho2006 <andreynho2006@gmail.com>
2018-08-26 16:58:45 +0300

    make a new variable to hold the id of the read more entry

--- controllers/blog.php
+++ controllers/blog.php
@@ -4,16 +4,25 @@ include_once "models/Blog_Entry_Table.class.php";
 
 $entryTable = new Blog_Entry_Table( $db );
 
-//$entries is the PDOStatement returned from getAllEntries
-$entries = $entryTable->getAllEntries();
-
 //fetch data from the first row as StdClass object
 //$oneEntry = $entries->fetchObject();
 
 //print the object
 //$test = print_r( $oneEntry, true );
 
-//load the view
-$blogOutput = include_once "views/list-entries-html.php";
+$isEntryClicked = isset( $_GET['id'] );
+
+if ( $isEntryClicked ) {
+    //show one entry ... soon
+    $entriId = $_GET['id'];
+    $blogOutput = "will soon show entry with entry_id = \$entryId";
+} else {
+    //list all entries
+    //$entries is the PDOStatement returned from getAllEntries
+    $entries = $entryTable->getAllEntries();
+    
+    //load the view
+    $blogOutput = include_once "views/list-entries-html.php";
+}
 
 return "<pre>$blogOutput</pre>";

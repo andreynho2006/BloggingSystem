c162e9bbd069d5bb908f592bd9c42c4c2819ea05
andreynho2006 <andreynho2006@gmail.com>
2018-08-26 09:21:44 +0300

    update controller to return all blog entries

--- controllers/blog.php
+++ controllers/blog.php
@@ -8,10 +8,12 @@ $entryTable = new Blog_Entry_Table( $db );
 $entries = $entryTable->getAllEntries();
 
 //fetch data from the first row as StdClass object
-$oneEntry = $entries->fetchObject();
+//$oneEntry = $entries->fetchObject();
 
 //print the object
-$test = print_r( $oneEntry, true );
+//$test = print_r( $oneEntry, true );
 
-//return the printed object to index to see in the browser
-return "<pre>$test</pre>";
+//load the view
+$blogOutput = include_once "views/list-entries-html.php";
+
+return "<pre>$blogOutput</pre>";

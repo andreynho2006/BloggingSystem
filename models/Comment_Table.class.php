9f365062f718d3d848c6b393b263fc0b85ea4fe0
andreynho2006 <andreynho2006@gmail.com>
2018-08-27 18:43:57 +0300

    refactor

--- models/Comment_Table.class.php
+++ models/Comment_Table.class.php
@@ -5,7 +5,7 @@ include_once "models/Table.class.php";
 class Comment_Table extends Table {
 
     //declare a new method inside the Comment_Table class
-    public function savecomment ( $entryId, $author, $txt ) {
+    public function saveComment ( $entryId, $author, $txt ) {
         $sql = "INSERT INTO comment ( entry_id, author, txt ) VALUES (?, ?, ?)";
         $data = array( $entryId, $author, $txt );
         $statement = $this->makeStatement( $sql, $data );

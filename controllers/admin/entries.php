d0b92bfd3a80502a96a4de8f9aab2ee68a6b5e1e
andreynho2006 <andreynho2006@gmail.com>
2018-08-27 08:35:06 +0300

    make a new Blog_Entry_Table object to get all entries

--- controllers/admin/entries.php
+++ controllers/admin/entries.php
@@ -1,3 +1,12 @@
 <?php
 
-return "<h1>entries controller loaded!</h1>";
+include_once "models/Blog_Entry_Table.class.php";
+
+$entryTable = new Blog_Entry_Table( $db );
+
+//get a PDOStatement object to acces all the entries
+$allEntries = $entryTable->getAllEntries();
+
+$entriesAsHTML = include_once "views/admin/entries-html.php";
+
+return $entriesAsHTML;

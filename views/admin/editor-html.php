3857b53bee91fd169790603e78ad10553f6fb6cf
andreynho2006 <andreynho2006@gmail.com>
2018-08-27 08:45:05 +0300

    check if a entryData exista.if not display blank fields, if yes display the content from database

--- views/admin/editor-html.php
+++ views/admin/editor-html.php
@@ -1,13 +1,26 @@
 <?php
 
+//check if required data is available
+$entryDataFound = isset ( $entryData );
+if ( $entryDataFound === false ) {
+    //default values for an empty editor
+    $entryData = new StdClass();
+    $entryData->entry_id = 0;
+    $entryData->title = "";
+    $entryData->entry_text = "";
+}
+
+//change object properties in <input> and <textarea>
 return "
 <form method='post' action='admin.php?page=editor' id='editor'>
+    <input type='hidden' name='id' value='$enryData->entry_id' />
     <fieldset>
         <legend>New entry submission</legend>
         <label>Title</label>
-        <input type='text' name='title' maxlength='150' />
+        <input type='text' name='title' maxlength='150' value='$entryData->title' />
+
         <label>Entry</label>
-        <textarea name='entry'></textarea>
+        <textarea name='entry'>$entryData->entry_text</textarea>
 
         <fieldset id='editor-buttons'>
             <input type='submit' name='action' value='save' />

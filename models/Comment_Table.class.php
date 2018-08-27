3cf3250b359e47f101e84947b57a03a0a6fd0cd2
andreynho2006 <andreynho2006@gmail.com>
2018-08-27 18:43:38 +0300

    add a new method to save a comment

--- models/Comment_Table.class.php
+++ models/Comment_Table.class.php
@@ -4,4 +4,11 @@ include_once "models/Table.class.php";
 
 class Comment_Table extends Table {
 
+    //declare a new method inside the Comment_Table class
+    public function savecomment ( $entryId, $author, $txt ) {
+        $sql = "INSERT INTO comment ( entry_id, author, txt ) VALUES (?, ?, ?)";
+        $data = array( $entryId, $author, $txt );
+        $statement = $this->makeStatement( $sql, $data );
+        return $statement;
+    }
 }

7884f72ff07b034f9da13e32538e3a0fe1656713
andreynho2006 <andreynho2006@gmail.com>
2018-08-27 17:32:15 +0300

    make a function to check if a title was submited

--- js/editor.js
+++ js/editor.js
@@ -1,5 +1,19 @@
 function init() {
-    console.log("you browser support DOMContentLoaded");
+    var editorForm = document.querySelector("form#editor");
+    editorForm.addEventListener("submit", checkTitle, false);
 }
 
 document.addEventListener("DOMContentLoaded", init, false);
+
+function checkTitle(event) {
+    var title = document.querySelector("input[name='title'");
+    var warning = document.querySelector("form 3title-warning");
+
+    //if title is empty
+    if(title.value === "") {
+        //preventdefault, ie dont't submit the form
+        event.preventDefault();
+        //display a warning
+        warning.innerHTML = "*You must write a title for the entry";
+    }
+}

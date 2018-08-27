fd7ad3217ff106d9c719a7aee03bdbe4343c9b43
andreynho2006 <andreynho2006@gmail.com>
2018-08-27 18:37:59 +0300

    make a new class Table

---
+++ models/Table.class.php
@@ -0,0 +1,22 @@
+<?php
+
+class Table {
+    //protected, not private
+    protected $db;
+
+    public function __construct ( $db ) {
+        $this->db = $db;
+    }
+
+    //protected, not private
+    protected function makeStatement ( $sql, $data = NULL ) {
+        $statement = $this->db->prepare( $sql );
+    try {
+        $statement->execute( $data );
+    } catch ( Exception $e ) {
+        $exceptionMessage = "<p>You tried to run this sql: $sql</p>
+                             <p>Exception: $e</p>";
+        trigger_error( $exceptionMessage );
+    }
+    return $statement;
+}

f128912f7a22e139b847f71825c221baef9e02db
andreynho2006 <andreynho2006@gmail.com>
2018-08-28 06:58:39 +0300

    make a new function to get the details of all comments of a particular entry

--- models/Comment_Table.class.php
+++ models/Comment_Table.class.php
@@ -11,4 +11,11 @@ class Comment_Table extends Table {
         $statement = $this->makeStatement( $sql, $data );
         return $statement;
     }
+
+    public function getAllById ( $id ) {
+        $sql = "SELECT author, txt, date FROM comment WHERE entry_id = ? ORDER BY comment_id DESC";
+        $data = array( $id );
+        $statement =$this->makeStatement( $sql, $data );
+        return $statement;
+    }
 }

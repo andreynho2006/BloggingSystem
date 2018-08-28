cdaf3b3a632dba0345e66775cfdb3d89522e1778
andreynho2006 <andreynho2006@gmail.com>
2018-08-28 06:53:12 +0300

    create a new comment and save it

--- controllers/comments.php
+++ controllers/comments.php
@@ -1,5 +1,15 @@
 <?php
 
+//include class definition
+include_once "models/Comment_Table.class.php";
+
+//create a new object , pass it a PDO Database connection object
+$commentTable = new Comment_Table( $db );
+
+//insert a test comment for entry_id = 1
+//assuming an entry_id of 1
+$commentTable->saveComment( 1, "me", "testing, testing" );
+
 $comments = include_once "views/comment-form-html.php";
 
 return $comments;
--- models/Table.class.php
+++ models/Table.class.php
@@ -11,12 +11,13 @@ class Table {
     //protected, not private
     protected function makeStatement ( $sql, $data = NULL ) {
         $statement = $this->db->prepare( $sql );
-    try {
-        $statement->execute( $data );
-    } catch ( Exception $e ) {
-        $exceptionMessage = "<p>You tried to run this sql: $sql</p>
-                             <p>Exception: $e</p>";
-        trigger_error( $exceptionMessage );
+        try {
+            $statement->execute( $data );
+        } catch ( Exception $e ) {
+            $exceptionMessage = "<p>You tried to run this sql: $sql</p>
+                                <p>Exception: $e</p>";
+            trigger_error( $exceptionMessage );
+        }
+        return $statement;
     }
-    return $statement;
 }

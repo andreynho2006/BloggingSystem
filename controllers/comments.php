ca4e0afd112b1accf5abfc8118ced84fa592d8c1
andreynho2006 <andreynho2006@gmail.com>
2018-08-28 07:13:16 +0300

    hook the model with the views comments-html

--- controllers/comments.php
+++ controllers/comments.php
@@ -5,19 +5,10 @@ include_once "models/Comment_Table.class.php";
 
 //create a new object , pass it a PDO Database connection object
 $commentTable = new Comment_Table( $db );
-
-//query database
-$allComments = $commentTable->getAllById ( $entryId );
-//get first row as a StdClass object
-$firstComment = $allComments->fetchObject();
-
-$testOutput = print_r( $firstComment, true );
-die("<pre>$testOutput</pre>");
-
-//insert a test comment for entry_id = 1
-//assuming an entry_id of 1
-//$commentTable->saveComment( 1, "me", "testing, testing" );
-
 $comments = include_once "views/comment-form-html.php";
+
+$allComments = $commentTable->getAllById( $entryId );
+
+$comments .= include_once "views/comments-html.php";
 
 return $comments;

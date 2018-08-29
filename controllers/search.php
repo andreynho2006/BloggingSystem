beaa2367580689b6d46fb5e3454f4a786f355051
andreynho2006 <andreynho2006@gmail.com>
2018-08-29 07:08:09 +0300

    test a search to see the output

--- controllers/search.php
+++ controllers/search.php
@@ -1,3 +1,19 @@
 <?php
 
-return "You just searched for something";
+//load the model
+include_once "models/Blog_Entry_Table.class.php";
+$blogTable = new Blog_Entry_Table( $db );
+
+//get PDOStatement object from model
+$searchData = $blogTable->searchEntry( "YelpCamp" );
+
+//get first row from result set
+$firstResult = $searchData->fetchObject();
+
+//inspect first row
+$searchOutput = print_r($firstResult, true);
+$searchForm = include_once "views/search-form-html.php";
+$searchOutput .= $searchForm;
+
+//display all output on index.php
+return $searchOutput;

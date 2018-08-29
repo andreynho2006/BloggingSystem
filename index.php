f9c45b806c06ba7a4f543554c563277f54230995
andreynho2006 <andreynho2006@gmail.com>
2018-08-29 06:53:19 +0300

    load the controller search if search form was submitted; blog is default

--- index.php
+++ index.php
@@ -14,12 +14,23 @@ $dbPassword = "";
 $db = new PDO( $dbInfo, $dbUser, $dbPassword );
 $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 
+$pageRequested = isset ( $_GET['page'] );
+//default controller is blog
+$controller = "blog";
+if ( $pageRequested ) {
+    //if user submited the search form
+    if ( $_GET['page'] === "search" ) {
+        //load the search by overwriting default controller
+        $controller = "search";
+    } 
+}
+
 //include search view before the blog controller
 $pageData->content .= include_once "views/search-form-html.php";
 
 //include blog controller
-$pageData->content .=include_once "controllers/blog.php";
-
+//pageData->content .=include_once "controllers/blog.php";
+$pageData->content .= include_once "controllers/$controller.php";
 
 
 $page = include_once "views/page.php";

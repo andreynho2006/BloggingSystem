41f3b7ecb4a42ba3a089484f8b1501dfbd827816
andreynho2006 <andreynho2006@gmail.com>
2018-08-29 07:19:55 +0300

    hook the result view

--- controllers/search.php
+++ controllers/search.php
@@ -4,16 +4,12 @@
 include_once "models/Blog_Entry_Table.class.php";
 $blogTable = new Blog_Entry_Table( $db );
 
-//get PDOStatement object from model
-$searchData = $blogTable->searchEntry( "YelpCamp" );
-
-//get first row from result set
-$firstResult = $searchData->fetchObject();
-
-//inspect first row
-$searchOutput = print_r($firstResult, true);
-$searchForm = include_once "views/search-form-html.php";
-$searchOutput .= $searchForm;
+$searchOutput = "";
+if ( isset ( $_POST['search-term'])) {
+    $searchTerm = $_POST['search-term'];
+    $searchData = $blogTable->searchEntry( $searchTerm );
+    $searchOutput = include_once "views/search-results-html.php";
+}
 
 //display all output on index.php
 return $searchOutput;

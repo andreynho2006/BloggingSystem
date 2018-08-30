6cbb52030f7e3da3088214cbf7b5ad6f9ff6d016
andreynho2006 <andreynho2006@gmail.com>
2018-08-30 07:02:40 +0300

    save the uploaded images with Uplader class

--- controllers/admin/images.php
+++ controllers/admin/images.php
@@ -1,11 +1,16 @@
 <?php
 
+include_once "models/Uploader.class.php";
 $imageSubmitted = isset( $_POST['new-image'] );
+
+//if the upload form was submitted
 if ( $imageSubmitted ) {
-    $testOutput = "<pre>";
-    $testOutput .=print_r($_FILES, true);
-    $testOutput .= "</pre>";
-return $testOutput;
+    //create an Uploader object
+    $uploader = new Uploader( 'image-data' );
+    //indicate destination folder on server
+    $uploader->saveIn( "img" );
+    $uploader->save();
+    $uploadMessage = "file probably uploaded!";
 }
 
 $imageManagerHTML = include_once "views/admin/images-html.php";

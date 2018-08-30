0b88535ed06e2079be82a9984d9d533e12a41352
andreynho2006 <andreynho2006@gmail.com>
2018-08-30 06:57:30 +0300

    test the image uploaded data

--- controllers/admin/images.php
+++ controllers/admin/images.php
@@ -1,4 +1,12 @@
 <?php
 
+$imageSubmitted = isset( $_POST['new-image'] );
+if ( $imageSubmitted ) {
+    $testOutput = "<pre>";
+    $testOutput .=print_r($_FILES, true);
+    $testOutput .= "</pre>";
+return $testOutput;
+}
+
 $imageManagerHTML = include_once "views/admin/images-html.php";
 return $imageManagerHTML;

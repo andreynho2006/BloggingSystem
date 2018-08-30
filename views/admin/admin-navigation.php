263b20a4d5ddc261975bbb077f8bdeaff39887b0
andreynho2006 <andreynho2006@gmail.com>
2018-08-30 06:42:33 +0300

    make a new navigation element for the uploading images page

--- views/admin/admin-navigation.php
+++ views/admin/admin-navigation.php
@@ -4,4 +4,5 @@ return "
 <nav id='admin-navigatioin'>
     <a href='admin.php?page=entries'>All entries</a>
     <a href='admin.php?page=editor'>Editor</a>
+    <a href='admin.php?page=images'>Image manager</a>
 </nav>";

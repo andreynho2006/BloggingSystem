dda948d98281e321f1a3a69984fd3c595ec3e77a
andreynho2006 <andreynho2006@gmail.com>
2018-09-01 08:24:47 +0300

    change the destination for the file uploader to see errors

--- controllers/admin/images.php
+++ controllers/admin/images.php
@@ -9,8 +9,16 @@ if ( $imageSubmitted ) {
     $uploader = new Uploader( 'image-data' );
     //indicate destination folder on server
     $uploader->saveIn( "img" );
-    $uploader->save();
-    $uploadMessage = "file probably uploaded!";
+    //try to save the upload file
+    try{
+        $uploader->save();
+        //create an upload message that confirms succesuful upload
+        $uploadMessage = "file uploaded!";
+        //catch any exception thrown
+    } catch ( Exception $exception ) {
+        //use the exception to create an upload message
+        $uploadMessage = $exception->getMessage();
+    }
 }
 
 $imageManagerHTML = include_once "views/admin/images-html.php";

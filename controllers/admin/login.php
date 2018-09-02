734d4eeeffc29a3313f8d2b69085ddb5aed629cc
andreynho2006 <andreynho2006@gmail.com>
2018-09-02 10:20:54 +0300

    refactor login form

--- controllers/admin/login.php
+++ controllers/admin/login.php
@@ -1,4 +1,9 @@
 <?php
 
+$loginFormSubmitted = isset( $_POST['log-in']);
+if( $loginFormSubmitted ) {
+    $admin->login();
+}
+
 $view = include_once "views/admin/login-form-html.php";
 return $view;

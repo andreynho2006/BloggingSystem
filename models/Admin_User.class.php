b4306f241a0c1c951b4add0e53d14a8fb003490d
andreynho2006 <andreynho2006@gmail.com>
2018-09-02 10:27:09 +0300

    use session global var to stay logged in

--- models/Admin_User.class.php
+++ models/Admin_User.class.php
@@ -1,17 +1,28 @@
 <?php
 
 class Admin_User {
+
     private $loggedIn = false;
 
+    public function __construct() {
+        session_start();
+    }
+
     public function isLoggedIn() {
-        return $this->loggedIn;
+        $sessionIsSet = isset( $_SESSION['logged_in']);
+        if( $sessionIsSet ) {
+            $out = $_SESSION['logged_in'];
+        } else {
+            $out = false;
+        }
+        return $out;
     }
 
     public function login() {
-        $this->loggedIn = true;
+        $_SESSION['logged_in'] = true;
     }
 
     public function logout() {
-        $this->loggedIn = false;
+        $_SESSION['logged_in'] = false;
     }
 }

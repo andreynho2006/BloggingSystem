3e0b7c8a0b885ce58e728c44d55ceeea1b1d0921
andreynho2006 <andreynho2006@gmail.com>
2018-09-02 10:44:08 +0300

    check if user exists in database when form is submitted

--- controllers/admin/login.php
+++ controllers/admin/login.php
@@ -1,8 +1,21 @@
 <?php
 
+include_once "models/Admin_Table.class.php";
+
 $loginFormSubmitted = isset( $_POST['log-in']);
 if( $loginFormSubmitted ) {
-    $admin->login();
+    //$admin->login();
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    //create an object for comunicating with the database table
+    $adminTable = new Admin_Table( $db );
+    try {
+        //try to login user
+        $adminTable->checkCredentials( $email, $password );
+        $admin->login();
+    } catch ( Exception $e ) {
+        //login failed
+    }
 }
 
 $loggingOut = isset( $_POST['logout']);
